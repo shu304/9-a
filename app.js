0ee59ed396282b5252dd9bf91dd81e2a598d8b30
shu <tyaro.rona@gmail.com>
2026-07-07 15:33:52 +0900

    15:33

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
-const PORT = 3000;
+const PORT = 3001;
 
 /* =====================
    API
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+<html lang="ja">
+<head>
+  <meta charset="UTF-8" />
+  <title>習慣管理アプリ</title>
+</head>
+<body>
+  <div id="root"></div>
+</body>
+</html>
